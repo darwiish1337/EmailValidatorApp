09e8edfdf06794572a754faa96dd0af6a3d38236
darwiish1337 <mdarwiish009@gmail.com>
2024-06-10 14:40:55 +0300

    Add project files.

---
+++ EmailValidatorApp/EmailValidator.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EmailValidatorApp
+{
+    public class EmailValidator : IUserValidator<IdentityUser>
+    {
+        private static string[] AllowedDomains = new[]
+        {
+            "gmail.com",
+            "hotmail.com",
+            "outlook.com",
+            "yahoo.com",
+            "msn.com",
+            "icloud.com",
+        };
+
+        private static IdentityError error
+            = new IdentityError { Description = "Email address domain not allowed" };
+
+        public EmailValidator(ILookupNormalizer normalizer)
+        {
+            Normalizer = normalizer;
+        }
+
+        private ILookupNormalizer Normalizer { get; set; }
+
+        public async Task<IdentityResult> ValidateAsync(UserManager<IdentityUser> manager, IdentityUser user)
+        {
+            string normalizedEmail = Normalizer.NormalizeEmail(user.Email);
+
+            if (AllowedDomains.Any(domain =>
+                    normalizedEmail.ToLower()
+                                   .EndsWith($"@{domain}")))
+            {
+                return IdentityResult.Success;
+            }
+            return IdentityResult.Failed(error);
+        }
+    }
+}
